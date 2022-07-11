b954e5f4a2a8a3291b460b818e749991bfbaaa3f
John Chaffee <johnchaffee@mac.com>
2022-07-11 08:53:49 -0700

    Test postman-params

---
+++ content/postman-params/messages/whatsapp.js
@@ -0,0 +1,21 @@
+postmanParams = {
+  url: "https://api.twilio.com/2010-04-01/Accounts/{{AccountSid}}/Messages.json",
+  method: "POST",
+  pathParams: [
+    { key: "AccountSid", value: "{{AccountSid}}" },
+  ],
+  queryParams: [
+    { key: "To", value: "whatsapp:+12063996576" },
+    { key: "From", value: "whatsapp:+14155238886" },
+    { key: "Body", value: "Hello from WhatsApp!" },
+    { key: "StatusCallback", value: "{{StatusCallback}}" },
+    { key: "MessagingServiceSid", value: "" },
+  ],
+  headerParams: [
+    { key: "Content-Type", value: "application/x-www-form-urlencoded" },
+  ],
+  authParams: [
+    { key: "username", value: "{{AccountSid}}" },
+    { key: "password", value: "{{AuthToken}}" },
+  ],
+}
